bd69f1dda963ffcaabbc125dbcd6f82f886ab6df
groundnuty <groundnuty@gmail.com>
2019-06-05 10:56:37 +0200

    made repository-prod.sh and dev scripts macos and linux compatibile

--- stable/repository-prod.sh
+++ stable/repository-prod.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+aliases() {
+  case $( uname -s ) in
+    Linux)
+          _sed=sed
+          ;;
+    Darwin)
+          _sed=gsed
+          ;;
+  esac
+}
+aliases
+
+
 while read f ; do
   sed -i 's#\(repository:\) file://../.*#\1 https://onedata.github.io/charts/#' $f ;
 done < <(find . -name requirements.yaml)
