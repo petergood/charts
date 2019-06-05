e2e4dcef46f3a92b703725a5ec4492554fb01d06
groundnuty <groundnuty@gmail.com>
2019-06-05 10:56:37 +0200

    made repository-prod.sh and dev scripts macos and linux compatibile

--- stable/repository-dev.sh
+++ stable/repository-dev.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+aliases() {
+  case $( uname -s ) in
+    Linux)
+          _sed=sed
+          ;;
+    Darwin)
+          _sed=gsed
+          ;;
+  esac
+}
+aliases
+
 while read f ; do
   while read l ; do
     sed  -i '/- name: '$l'/,/repository/{s#\(repository:\).*onedata.*#\1 file://../'$l'#}'  $f ;
